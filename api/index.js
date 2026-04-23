35c57de51dac00c8a52d7d8011c41a7838c7f7d1
bamideletitilope <bamideletitilope94@gmail.com>
2026-04-23 13:09:37 +0100

    fix:create vercel.json file and remove server.js file for easy deployment on vercel

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const profileRoute = require('../api/profiles');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', profileRoute);
+
+//NO app.listen
+module.exports = app;
