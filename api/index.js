c137afc6ad090b15b21b5f5325389fa226412c04
bamideletitilope <bamideletitilope94@gmail.com>
2026-04-23 14:46:58 +0100

    fix: vercel servelless deployment

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
-const profileRoute = require('./profiles');
+const profileRoute = require('../api/profiles');
 
 const app = express();
 
